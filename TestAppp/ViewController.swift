3beab9628a9141e23f70d72c4a2277af7315a9c2
Ahmad Owais <ahmadowais@Ahmads-MacBook-Pro.local>
2017-08-02 15:53:02 +0400

    Initial Commit

---
+++ TestAppp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppp
+//
+//  Created by Ahmad Owais on 8/2/17.
+//  Copyright © 2017 Ahmad Owais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
